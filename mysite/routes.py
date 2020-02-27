dc90d7d99ef2399e2e9866fb323f9649a076b743
bad-data <minusben@gmail.com>
2020-02-27 03:36:44 -0500

    optimized

--- mysite/routes.py
+++ mysite/routes.py
@@ -2,10 +2,6 @@ from flask_app import app
 from flask import redirect, url_for, render_template
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-
-@app.route('/index')
 def index():
     return render_template('index.html', title='Index Page')
 
