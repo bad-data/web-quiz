48ff54e9aa3370a20d7d849eeb29bbbc5b5d0823
bad-data <minusben@gmail.com>
2020-02-27 02:25:33 -0500

    testing js

--- mysite/js/myFunctions.js
+++ mysite/js/myFunctions.js
@@ -1,3 +1,7 @@
-document.open()
-document.write("this is printed from javascript!")
-document.close()
+function hello() {
+    document.open();
+    document.write("this is printed from javascript!");
+    document.close();
+}
+
+hello();
